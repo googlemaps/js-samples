bff5c763eeb622d992a0b742f6bb096e43a7e787
Justin <jpoehnelt@google.com>
2019-11-19 15:48:35 -0800

    fix: region tags should be unique and start with maps_

---
+++ build/test_tags.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+tmp=$(mktemp)
+
+grep -or "\[START [a-zA-Z0-9_]*\]" samples/**/src >$tmp
+
+# use associative array to track tags
+declare -A tags
+
+# track errors
+errors_duplicate=()
+errors_lint=()
+
+while read l; do
+    path=$(echo "$l" | cut -d: -f1)
+
+    # use package name without file extension
+    package=$(dirname $path)
+
+    # region tag including START
+    region=$(echo "$l" | cut -d: -f2)
+
+    if [ "${tags[$region]}" == "" ]; then
+        tags[$region]=$package
+    else
+        if [ "${tags[$region]}" != "$package" ]; then
+            errors_duplicate+=("duplicate for $region: $package conflicts with ${tags[$region]}")
+        fi
+    fi
+
+    # Check if begins with [START maps_*]
+    if [ "${region:7:5}" != "maps_" ]; then
+        errors_lint+=("region tag should start with maps_: $region")
+    fi
+done <$tmp
+
+if [ "${#errors_duplicate[@]}" == "0" ] && [ "${#errors_lint[@]}" == "0" ]; then
+    exit 0
+fi
+
+# print errors
+(
+    IFS=$'\n'
+    echo "${errors_duplicate[*]}"
+    echo "${errors_lint[*]}"
+)
