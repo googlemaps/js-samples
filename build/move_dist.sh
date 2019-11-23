6e5f0a136c721f346a6489ce66aa5c79eebe1247
Justin Poehnelt <jpoehnelt@google.com>
2019-11-22 17:25:40 -0800

    feat: move index.js to dist

--- build/move_dist.sh
+++ build/move_dist.sh
@@ -2,5 +2,7 @@
 
 set -euo pipefail
 
-mkdir -p ../../dist/samples/$npm_package_name 
-cp dist/* ../../dist/samples/$npm_package_name/
+mkdir -p ../../dist/samples/$npm_package_name/src
+
+cp dist/* ../../dist/samples/$npm_package_name/dist
+cp src/index.js ../../dist/samples/$npm_package_name/src/index.js
