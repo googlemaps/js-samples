ab7458d5fe746d313224911763128d96c1c82063
Justin Poehnelt <jpoehnelt@google.com>
2019-11-23 04:40:10 +0000

    Deploy googlemaps/js-samples to github.com/googlemaps/js-samples.git:master
    Former-commit-id: efa45ca1a3dc1d731498f4cfd0b0279c2acc705b
    Former-commit-id: c53cf467978de69d65c5fc99cf00d65fc2e47690

---
+++ samples/rectangle-simple/src/index.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START maps_rectangle_simple]
+// This example adds a red rectangle to a map.
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 11,
+    center: { lat: 33.678, lng: -116.243 },
+    mapTypeId: "terrain"
+  });
+
+  var rectangle = new google.maps.Rectangle({
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    map: map,
+    bounds: {
+      north: 33.685,
+      south: 33.671,
+      east: -116.234,
+      west: -116.251
+    }
+  });
+}
+// [END maps_rectangle_simple]
+export { initMap };
