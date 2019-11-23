2d650f45a8487c03fed4cea4979231a1245482da
Justin Poehnelt <jpoehnelt@google.com>
2019-11-23 00:24:31 -0600

    trigger change

--- samples/add-map/src/index.js
+++ samples/add-map/src/index.js
@@ -18,9 +18,9 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = { lat: -25.344, lng: 131.036 };
+  const uluru = { lat: -25.344, lng: 131.036 };
   // The map, centered at Uluru
-  var map = new google.maps.Map(document.getElementById("map"), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: uluru
   });
