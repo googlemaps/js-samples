8365b5e5ffb58ddd00269bca9bd8df5f504e9247
Justin <jpoehnelt@google.com>
2019-12-11 08:35:57 -0800

    feat: upload public folder to gcs

---
+++ .travis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ "$TRAVIS_BRANCH" != "master" ]; then 
+    yarn run build;
+    yarn run test; 
+    yarn run deploy:extract;
+    cp .travis.yml public/.travis.yml;
+fi
