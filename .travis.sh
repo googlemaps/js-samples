204de5a96cf51a20d86b408c961664ef95e882e2
Justin Poehnelt <jpoehnelt@google.com>
2019-12-12 16:35:13 -0700

    fix: master build deploy path
    Former-commit-id: 34ce682606c02a9aee55da816ac7e80540acdf85
    Former-commit-id: 71fd84d884b6c4acc9a495499020f5ea6e16fdfe

--- .travis.sh
+++ .travis.sh
@@ -6,9 +6,18 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
     yarn run build;
     yarn run test; 
 
-    yarn run deploy:public;
+    # Extracted files for GCS
+    yarn run extract:public;
 
+    # Extracted files for Github 
+    yarn run extract:master;
+
+    # Copy the files over for publishing to master branch
+    cp .nojeklyll master/.nojeklyll
     cp .travis.yml master/.travis.yml
     cp .travis.sh master/.travis.sh
+    cp CONTRIB.md master/CONTRIB.md
+    cp LICENSE master/LICENSE
     cp README.md master/README.md
+
 fi
