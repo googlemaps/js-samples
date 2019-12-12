9b57aed4c6ee18f34c2bb6c8e9d7f104f79d6481
Justin Poehnelt <jpoehnelt@google.com>
2019-12-12 22:56:53 +0000

    Deploy googlemaps/js-samples to github.com/googlemaps/js-samples.git:master
    Former-commit-id: c6ca81c6515d5d2934340aefbb355067d7d25594
    Former-commit-id: 334adecb4294fd174c7139365d4cf641c4200202

--- .travis.sh
+++ .travis.sh
@@ -5,5 +5,10 @@ set -euo pipefail
 if [ "$TRAVIS_BRANCH" != "master" ]; then 
     yarn run build;
     yarn run test; 
-    yarn run deploy:extract;
+
+    yarn run deploy:public;
+
+    cp .travis.yml master/.travis.yml
+    cp .travis.sh master/.travis.sh
+    cp README.md master/README.md
 fi
