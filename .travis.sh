d409781a95a61dd06b187a4216ffa361a57c065e
Justin Poehnelt <jpoehnelt@google.com>
2019-12-12 14:38:44 -0700

    fix: improve publish to master (#91)
    Former-commit-id: 9982f9cec223ce882fa4d97be8c3a55f36231148
    Former-commit-id: dc9d919358ccdce731d0c3746aa30f2120f9f4dd

--- .travis.sh
+++ .travis.sh
@@ -6,6 +6,4 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
     yarn run build;
     yarn run test; 
     yarn run deploy:extract;
-    cp .travis.yml public/.travis.yml;
-    cp .travis.sh public/.travis.sh;
 fi
