fe7d3186cdea27a4d303aca486c5f3f2b80cdf56
Justin Poehnelt <jpoehnelt@google.com>
2020-05-15 09:09:42 -0700

    fix: remove license headers from outputs (#177)

--- dist/samples/overlay-simple/app.js
+++ dist/samples/overlay-simple/app.js
@@ -1,21 +1,6 @@
 (function(exports) {
   "use strict";
 
-  /*
-   * Copyright 2019 Google LLC. All Rights Reserved.
-   *
-   * Licensed under the Apache License, Version 2.0 (the "License");
-   * you may not use this file except in compliance with the License.
-   * You may obtain a copy of the License at
-   *
-   *     http://www.apache.org/licenses/LICENSE-2.0
-   *
-   * Unless required by applicable law or agreed to in writing, software
-   * distributed under the License is distributed on an "AS IS" BASIS,
-   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   * See the License for the specific language governing permissions and
-   * limitations under the License.
-   */
   // This example creates a custom overlay called USGSOverlay, containing
   // a U.S. Geological Survey (USGS) image of the relevant area on the map.
   // Set the custom overlay object's prototype to a new instance
