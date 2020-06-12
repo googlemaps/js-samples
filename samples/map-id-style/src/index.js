9f6f0f9fb6d10d6709243dcfd1661f67da17019c
Justin <jpoehnelt@google.com>
2020-06-12 07:52:27 -0700

    fix: update location of map

--- samples/map-id-style/src/index.js
+++ samples/map-id-style/src/index.js
@@ -18,8 +18,8 @@
 function initMap() {
   new google.maps.Map(document.getElementById("map"), {
     mapId: "8e0a97af9386fef",
-    center: { lat: 0, lng: 0 },
-    zoom: 4
+    center: { lat: 48.85, lng: 2.35 },
+    zoom: 12
   });
 }
 // [END maps_map_id_style]
