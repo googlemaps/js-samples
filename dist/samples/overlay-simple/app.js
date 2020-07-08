15c01dbc91e3ddf2829a2cc6258154051226c9ce
Justin Poehnelt <jpoehnelt@google.com>
2020-07-08 18:03:01 +0000

    fix: custom overlay prototype order (#208)
    Former-commit-id: 432d48d64df69121ca4115459cb4ca6a3758439e
    Former-commit-id: eac94f45ccb452aeeab3404c50cbb6b793f610dd

--- dist/samples/overlay-simple/app.js
+++ dist/samples/overlay-simple/app.js
@@ -9,10 +9,78 @@
   // google.maps.event.addDomListener().
   // Note that we set the prototype to an instance, rather than the
   // parent class itself, because we do not wish to modify the parent class.
-  // Initialize the map and the custom overlay.
+  var overlay;
+  /** Initialize the map and the custom overlay. */
 
   function initMap() {
+    /** @constructor */
+    function USGSOverlay(bounds, image, map) {
+      // Initialize all properties.
+      this.bounds_ = bounds;
+      this.image_ = image;
+      this.map_ = map; // Define a property to hold the image's div. We'll
+      // actually create this div upon receipt of the onAdd()
+      // method so we'll leave it null for now.
+
+      this.div_ = null; // Explicitly call setMap on this overlay.
+
+      this.setMap(map);
+    }
+
     USGSOverlay.prototype = new google.maps.OverlayView();
+    /**
+     * onAdd is called when the map's panes are ready and the overlay has been
+     * added to the map.
+     */
+
+    USGSOverlay.prototype.onAdd = function() {
+      var div = document.createElement("div");
+      div.style.borderStyle = "none";
+      div.style.borderWidth = "0px";
+      div.style.position = "absolute"; // Create the img element and attach it to the div.
+
+      var img = document.createElement("img");
+      img.src = this.image_;
+      img.style.width = "100%";
+      img.style.height = "100%";
+      img.style.position = "absolute";
+      div.appendChild(img);
+      this.div_ = div; // Add the element to the "overlayLayer" pane.
+
+      var panes = this.getPanes();
+      panes.overlayLayer.appendChild(div);
+    };
+
+    USGSOverlay.prototype.draw = function() {
+      // We use the south-west and north-east
+      // coordinates of the overlay to peg it to the correct position and size.
+      // To do this, we need to retrieve the projection from the overlay.
+      var overlayProjection = this.getProjection(); // Retrieve the south-west and north-east coordinates of this overlay
+      // in LatLngs and convert them to pixel coordinates.
+      // We'll use these coordinates to resize the div.
+
+      var sw = overlayProjection.fromLatLngToDivPixel(
+        this.bounds_.getSouthWest()
+      );
+      var ne = overlayProjection.fromLatLngToDivPixel(
+        this.bounds_.getNorthEast()
+      ); // Resize the image's div to fit the indicated dimensions.
+
+      var div = this.div_;
+      div.style.left = sw.x + "px";
+      div.style.top = ne.y + "px";
+      div.style.width = ne.x - sw.x + "px";
+      div.style.height = sw.y - ne.y + "px";
+    };
+    /** The onRemove() method will be called automatically from the API if
+     * we ever set the overlay's map property to 'null'.
+     */
+
+    USGSOverlay.prototype.onRemove = function() {
+      this.div_.parentNode.removeChild(this.div_);
+      this.div_ = null;
+    };
+
     var map = new google.maps.Map(document.getElementById("map"), {
       zoom: 11,
       center: {
@@ -31,73 +99,8 @@
       "javascript/examples/full/images/talkeetna.png"; // The custom USGSOverlay object contains the USGS image,
     // the bounds of the image, and a reference to the map.
 
-    exports.overlay = new USGSOverlay(bounds, srcImage, map);
+    overlay = new USGSOverlay(bounds, srcImage, map);
   }
-  /** @constructor */
 
-  function USGSOverlay(bounds, image, map) {
-    // Initialize all properties.
-    this.bounds_ = bounds;
-    this.image_ = image;
-    this.map_ = map; // Define a property to hold the image's div. We'll
-    // actually create this div upon receipt of the onAdd()
-    // method so we'll leave it null for now.
-
-    this.div_ = null; // Explicitly call setMap on this overlay.
-
-    this.setMap(map);
-  }
-  /**
-   * onAdd is called when the map's panes are ready and the overlay has been
-   * added to the map.
-   */
-
-  USGSOverlay.prototype.onAdd = function() {
-    var div = document.createElement("div");
-    div.style.borderStyle = "none";
-    div.style.borderWidth = "0px";
-    div.style.position = "absolute"; // Create the img element and attach it to the div.
-
-    var img = document.createElement("img");
-    img.src = this.image_;
-    img.style.width = "100%";
-    img.style.height = "100%";
-    img.style.position = "absolute";
-    div.appendChild(img);
-    this.div_ = div; // Add the element to the "overlayLayer" pane.
-
-    var panes = this.getPanes();
-    panes.overlayLayer.appendChild(div);
-  };
-
-  USGSOverlay.prototype.draw = function() {
-    // We use the south-west and north-east
-    // coordinates of the overlay to peg it to the correct position and size.
-    // To do this, we need to retrieve the projection from the overlay.
-    var overlayProjection = this.getProjection(); // Retrieve the south-west and north-east coordinates of this overlay
-    // in LatLngs and convert them to pixel coordinates.
-    // We'll use these coordinates to resize the div.
-
-    var sw = overlayProjection.fromLatLngToDivPixel(
-      this.bounds_.getSouthWest()
-    );
-    var ne = overlayProjection.fromLatLngToDivPixel(
-      this.bounds_.getNorthEast()
-    ); // Resize the image's div to fit the indicated dimensions.
-
-    var div = this.div_;
-    div.style.left = sw.x + "px";
-    div.style.top = ne.y + "px";
-    div.style.width = ne.x - sw.x + "px";
-    div.style.height = sw.y - ne.y + "px";
-  }; // The onRemove() method will be called automatically from the API if
-  // we ever set the overlay's map property to 'null'.
-
-  USGSOverlay.prototype.onRemove = function() {
-    this.div_.parentNode.removeChild(this.div_);
-    this.div_ = null;
-  };
-
-  exports.USGSOverlay = USGSOverlay;
   exports.initMap = initMap;
 })((this.window = this.window || {}));
--- samples/overlay-simple/src/index.js
+++ samples/overlay-simple/src/index.js
@@ -15,7 +15,6 @@
  */
 
 // [START maps_overlay_simple]
-// [START maps_overlay_simple_region_initialization]
 // This example creates a custom overlay called USGSOverlay, containing
 // a U.S. Geological Survey (USGS) image of the relevant area on the map.
 
@@ -28,10 +27,90 @@
 
 var overlay;
 
-// Initialize the map and the custom overlay.
+/** Initialize the map and the custom overlay. */
 function initMap() {
+  // [START maps_overlay_simple_region_constructor]
+  /** @constructor */
+  function USGSOverlay(bounds, image, map) {
+    // Initialize all properties.
+    this.bounds_ = bounds;
+    this.image_ = image;
+    this.map_ = map;
+
+    // Define a property to hold the image's div. We'll
+    // actually create this div upon receipt of the onAdd()
+    // method so we'll leave it null for now.
+    this.div_ = null;
+
+    // Explicitly call setMap on this overlay.
+    this.setMap(map);
+  }
+  // [END maps_overlay_simple_region_constructor]
+
   USGSOverlay.prototype = new google.maps.OverlayView();
 
+  // [START maps_overlay_simple_region_attachment]
+  /**
+   * onAdd is called when the map's panes are ready and the overlay has been
+   * added to the map.
+   */
+  USGSOverlay.prototype.onAdd = function() {
+    var div = document.createElement("div");
+    div.style.borderStyle = "none";
+    div.style.borderWidth = "0px";
+    div.style.position = "absolute";
+
+    // Create the img element and attach it to the div.
+    var img = document.createElement("img");
+    img.src = this.image_;
+    img.style.width = "100%";
+    img.style.height = "100%";
+    img.style.position = "absolute";
+    div.appendChild(img);
+
+    this.div_ = div;
+
+    // Add the element to the "overlayLayer" pane.
+    var panes = this.getPanes();
+    panes.overlayLayer.appendChild(div);
+  };
+  // [END maps_overlay_simple_region_attachment]
+
+  // [START maps_overlay_simple_region_drawing]
+  USGSOverlay.prototype.draw = function() {
+    // We use the south-west and north-east
+    // coordinates of the overlay to peg it to the correct position and size.
+    // To do this, we need to retrieve the projection from the overlay.
+    var overlayProjection = this.getProjection();
+
+    // Retrieve the south-west and north-east coordinates of this overlay
+    // in LatLngs and convert them to pixel coordinates.
+    // We'll use these coordinates to resize the div.
+    var sw = overlayProjection.fromLatLngToDivPixel(
+      this.bounds_.getSouthWest()
+    );
+    var ne = overlayProjection.fromLatLngToDivPixel(
+      this.bounds_.getNorthEast()
+    );
+
+    // Resize the image's div to fit the indicated dimensions.
+    var div = this.div_;
+    div.style.left = sw.x + "px";
+    div.style.top = ne.y + "px";
+    div.style.width = ne.x - sw.x + "px";
+    div.style.height = sw.y - ne.y + "px";
+  };
+  // [END maps_overlay_simple_region_drawing]
+
+  // [START maps_overlay_simple_region_removal]
+  /** The onRemove() method will be called automatically from the API if
+   * we ever set the overlay's map property to 'null'.
+   */
+  USGSOverlay.prototype.onRemove = function() {
+    this.div_.parentNode.removeChild(this.div_);
+    this.div_ = null;
+  };
+  // [END maps_overlay_simple_region_removal]
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 11,
     center: { lat: 62.323907, lng: -150.109291 },
@@ -52,82 +131,6 @@ function initMap() {
   // the bounds of the image, and a reference to the map.
   overlay = new USGSOverlay(bounds, srcImage, map);
 }
-// [END maps_overlay_simple_region_initialization]
 
-// [START maps_overlay_simple_region_constructor]
-/** @constructor */
-function USGSOverlay(bounds, image, map) {
-  // Initialize all properties.
-  this.bounds_ = bounds;
-  this.image_ = image;
-  this.map_ = map;
-
-  // Define a property to hold the image's div. We'll
-  // actually create this div upon receipt of the onAdd()
-  // method so we'll leave it null for now.
-  this.div_ = null;
-
-  // Explicitly call setMap on this overlay.
-  this.setMap(map);
-}
-// [END maps_overlay_simple_region_constructor]
-
-// [START maps_overlay_simple_region_attachment]
-/**
- * onAdd is called when the map's panes are ready and the overlay has been
- * added to the map.
- */
-USGSOverlay.prototype.onAdd = function() {
-  var div = document.createElement("div");
-  div.style.borderStyle = "none";
-  div.style.borderWidth = "0px";
-  div.style.position = "absolute";
-
-  // Create the img element and attach it to the div.
-  var img = document.createElement("img");
-  img.src = this.image_;
-  img.style.width = "100%";
-  img.style.height = "100%";
-  img.style.position = "absolute";
-  div.appendChild(img);
-
-  this.div_ = div;
-
-  // Add the element to the "overlayLayer" pane.
-  var panes = this.getPanes();
-  panes.overlayLayer.appendChild(div);
-};
-// [END maps_overlay_simple_region_attachment]
-
-// [START maps_overlay_simple_region_drawing]
-USGSOverlay.prototype.draw = function() {
-  // We use the south-west and north-east
-  // coordinates of the overlay to peg it to the correct position and size.
-  // To do this, we need to retrieve the projection from the overlay.
-  var overlayProjection = this.getProjection();
-
-  // Retrieve the south-west and north-east coordinates of this overlay
-  // in LatLngs and convert them to pixel coordinates.
-  // We'll use these coordinates to resize the div.
-  var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-  var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-
-  // Resize the image's div to fit the indicated dimensions.
-  var div = this.div_;
-  div.style.left = sw.x + "px";
-  div.style.top = ne.y + "px";
-  div.style.width = ne.x - sw.x + "px";
-  div.style.height = sw.y - ne.y + "px";
-};
-// [END maps_overlay_simple_region_drawing]
-
-// [START maps_overlay_simple_region_removal]
-// The onRemove() method will be called automatically from the API if
-// we ever set the overlay's map property to 'null'.
-USGSOverlay.prototype.onRemove = function() {
-  this.div_.parentNode.removeChild(this.div_);
-  this.div_ = null;
-};
-// [END maps_overlay_simple_region_removal]
 // [END maps_overlay_simple]
-export { overlay, initMap, USGSOverlay };
+export { initMap };
