763cf21a154b545d8993b16170d1e2c7e7bba390
Justin Poehnelt <jpoehnelt@google.com>
2020-08-03 18:32:40 -0600

    fix: shorten line (#221)

--- samples/programmatic-load-button/src/index.ts
+++ samples/programmatic-load-button/src/index.ts
@@ -19,6 +19,7 @@ let map: google.maps.Map;
 
 const center = { lat: 41.90476224706472, lng: 12.49822074385094 };
 const zoom = 14;
+const url = "https://maps.googleapis.com/maps/api/staticmap";
 
 // @ts-ignore google.maps.plugins
 const loader = new google.maps.plugins.loader.Loader({
@@ -29,7 +30,7 @@ const loader = new google.maps.plugins.loader.Loader({
 document.addEventListener("DOMContentLoaded", () => {
   const wrapper = document.getElementById("wrapper") as HTMLButtonElement;
 
-  wrapper.style.backgroundImage = `url(https://maps.googleapis.com/maps/api/staticmap?center=${center.lat},${center.lng}&zoom=${zoom}&scale=2&size=${wrapper.clientWidth}x${wrapper.clientHeight}&key=YOUR_API_KEY)`;
+  wrapper.style.backgroundImage = `url(${url}?center=${center.lat},${center.lng}&zoom=${zoom}&scale=2&size=${wrapper.clientWidth}x${wrapper.clientHeight}&key=YOUR_API_KEY)`;
 
   wrapper.addEventListener("click", () => {
     wrapper.remove();
