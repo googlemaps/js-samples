bad18f86b9ed60dc4f68662b49e7a2c697cd4f95
Justin Poehnelt <jpoehnelt@google.com>
2020-08-10 15:41:43 -0600

    fix: infowindow open (#230)
    Former-commit-id: e45102ea01406d71c810a434e5189808f66a07d4
    Former-commit-id: f6f8c3f4855f6f0d84a023e78be187ad9653ac46

--- dist/samples/elevation-simple/index.js
+++ dist/samples/elevation-simple/index.js
@@ -6,6 +6,7 @@ function initMap() {
   });
   const elevator = new google.maps.ElevationService();
   const infowindow = new google.maps.InfoWindow({});
+  infowindow.open(map);
   // Add a listener for the click event. Display the elevation for the LatLng of
   // the click inside the infowindow.
   map.addListener("click", event => {
