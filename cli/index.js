1c7441ae65a3aa558129c00f2af497e991aaaf11
Justin Poehnelt <jpoehnelt@google.com>
2020-08-20 15:56:27 -0600

    feat: enable immediate run and open (#247)

--- cli/index.js
+++ cli/index.js
@@ -1,11 +1,29 @@
 #!/usr/bin/env node
 
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const yargs = require("yargs");
 const path = require("path");
 const tar = require("tar");
 const tmp = require("tmp");
 const fs = require("fs");
 const fetch = require("node-fetch");
+const npm = require("npm");
+const openBrowser = require("open");
 
 const GITHUB_CONTENTS_API =
   "https://api.github.com/repos/googlemaps/js-samples/contents";
@@ -28,7 +46,14 @@ const getPackageFromRef = async (sample, ref, verbose) => {
 };
 
 const main = async argv => {
-  const { sample, ref, destination, verbose } = argv;
+  const {
+    sample,
+    ref,
+    destination,
+    verbose,
+    hot: shouldRun,
+    open: shouldOpen
+  } = argv;
   let file;
 
   if (ref) {
@@ -50,14 +75,23 @@ const main = async argv => {
   await tar.x({
     file,
     C: destination,
-    strict: true,
+    strict: true
   });
 
-  console.info(`Run the following to start the application:
+  process.chdir(destination);
 
-$ cd '${destination}';
-$ npm i;
-$ npm run dev;`);
+  if (shouldRun) {
+    npm.load({}, () => {
+      npm.commands.install([""], () => {
+        npm.commands["run-script"](["dev"]);
+        if (shouldOpen) {
+          setTimeout(() => {
+            openBrowser("http://localhost:8080");
+          }, 1000);
+        }
+      });
+    });
+  }
 };
 
 yargs
@@ -65,14 +99,9 @@ yargs
     "init <sample> [destination]",
     "initialize a sample skeleton",
     yargs => {
-      yargs.option("ref", {
-        alias: "r",
-        describe: "The git ref of git@github.com:googlemaps/js-samples.git",
-        default: "master"
-      }),
-        yargs.positional("sample", {
-          describe: "The sample identifier"
-        });
+      yargs.positional("sample", {
+        describe: "The sample identifier"
+      });
       yargs.positional("destination", {
         describe: "The destination folder for the sample",
         default: "."
@@ -84,4 +113,21 @@ yargs
     alias: "v",
     type: "boolean",
     description: "Run with verbose logging"
+  })
+  .option("hot", {
+    type: "boolean",
+    alias: "h",
+    description: "Immediately install and run application",
+    default: true
+  })
+  .option("open", {
+    type: "boolean",
+    alias: "o",
+    description: "Open application in browser",
+    default: true
+  })
+  .option("ref", {
+    alias: "r",
+    describe: "The git ref of git@github.com:googlemaps/js-samples.git",
+    default: "master"
   }).argv;
