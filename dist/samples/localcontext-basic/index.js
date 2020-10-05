00f962f3e87491d4c89ce2ef85fd7f6c076b41e8
Justin Poehnelt <jpoehnelt@google.com>
2020-10-05 14:01:50 -0600

    feat: add localcontext samples (#328)

---
+++ dist/samples/localcontext-basic/index.js
@@ -0,0 +1,14 @@
+let map;
+
+function initMap() {
+  const localContextMapView = new google.maps.localContext.LocalContextMapView({
+    element: document.getElementById("map"),
+    placeTypePreferences: ["restaurant", "tourist_attraction"],
+    maxPlaceCount: 12,
+  });
+  map = localContextMapView.map;
+  map.setOptions({
+    center: { lat: 51.507307, lng: -0.08114 },
+    zoom: 14,
+  });
+}
---
+++ samples/localcontext-basic/src/index.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START maps_js_localcontext_basic]
+let map: google.maps.Map;
+
+function initMap() {
+  // @ts-ignore beta feature not in type declarations
+  const localContextMapView = new google.maps.localContext.LocalContextMapView({
+    element: document.getElementById("map"),
+    placeTypePreferences: ["restaurant", "tourist_attraction"],
+    maxPlaceCount: 12,
+  });
+
+  map = localContextMapView.map;
+
+  map.setOptions({
+    center: { lat: 51.507307, lng: -0.08114 },
+    zoom: 14,
+  });
+}
+// [END maps_js_localcontext_basic]
+export { initMap };
