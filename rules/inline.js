d77970fdc639c2050b1f8abeb0b9db5240c686bb
Justin Poehnelt <jpoehnelt@google.com>
2020-10-12 12:28:14 -0600

    build: use async and promise for async calls (#351)

--- rules/inline.js
+++ rules/inline.js
@@ -19,39 +19,44 @@ var inline = require("web-resource-inliner");
 var path = require("path");
 
 function normalize(contents) {
-    return process.platform === "win32" ? contents.replace(/\r\n/g, "\n") : contents;
+  return process.platform === "win32"
+    ? contents.replace(/\r\n/g, "\n")
+    : contents;
 }
 
 function readFile(file) {
-    return normalize(fs.readFileSync(file, "utf8"));
+  return normalize(fs.readFileSync(file, "utf8"));
 }
 
 function writeFile(file, data) {
-    return fs.writeFileSync(file, normalize(data));
+  return fs.writeFileSync(file, normalize(data));
 }
 
-function main(args) {
-    let src = args[2];
-    let out = args[3];
-    
-    inline.html({
+async function main(args) {
+  let src = args[2];
+  let out = args[3];
+  
+  return new Promise((resolve, reject) => {
+    inline.html(
+      {
         fileContent: readFile(src),
         relativeTo: path.dirname(src),
         // only inline elements having data-inline
         scripts: false,
         images: false,
         links: false,
-        svg: false
-    },
-        function (err, result) {
-            if (!err) {
-                writeFile(out, result);
-            } else {
-                throw err;
-            }
+        svg: false,
+      },
+      function (err, result) {
+        if (!err) {
+          writeFile(out, result);
+          resolve(result);
+        } else {
+          reject(err);
         }
+      }
     );
-
+  });
 }
 
-main(process.argv)
+main(process.argv);
