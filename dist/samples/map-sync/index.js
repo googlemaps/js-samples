284eba013fb0cbc766ea932edfd453a150e61689
Justin <jpoehnelt@google.com>
2020-10-14 12:29:27 -0700

    fix: map-sync should actually be synchronous

--- dist/samples/map-sync/index.js
+++ dist/samples/map-sync/index.js
@@ -1,10 +1,6 @@
 // [START maps_map_sync]
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
+const map = new google.maps.Map(document.getElementById("map"), {
+  center: { lat: -34.397, lng: 150.644 },
+  zoom: 8,
+});
 // [END maps_map_sync]
