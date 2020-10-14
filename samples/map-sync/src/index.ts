a7ccd48d847171970a88c6d61b3d1ff35ff5bfe0
Justin Poehnelt <jpoehnelt@google.com>
2020-10-14 13:25:26 -0700

    fix: map-sync should actually be synchronous (#355)

--- samples/map-sync/src/index.ts
+++ samples/map-sync/src/index.ts
@@ -15,13 +15,10 @@
  */
 
 // [START maps_map_sync]
-let map: google.maps.Map;
+const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  center: { lat: -34.397, lng: 150.644 },
+  zoom: 8,
+});
 
-function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
 // [END maps_map_sync]
-export { initMap };
+export { map };
