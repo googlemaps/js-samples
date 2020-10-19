f1b8240a931cd1336b9a9604a3fb0be07db9aeec
Justin Poehnelt <jpoehnelt@google.com>
2020-10-19 08:56:45 -0700

    fix: move file contents and change font-size (#368)

--- dist/samples/layer-data-dragndrop/index.js
+++ dist/samples/layer-data-dragndrop/index.js
@@ -1,5 +1,123 @@
 // [START maps_layer_data_dragndrop]
-/* Map functions */
+let map;
+
+function initMap() {
+  // set up the map
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: new google.maps.LatLng(0, 0),
+    zoom: 4,
+  });
+}
+
+function loadGeoJsonString(geoString) {
+  try {
+    const geojson = JSON.parse(geoString);
+    map.data.addGeoJson(geojson);
+  } catch (e) {
+    alert("Not a GeoJSON file!");
+  }
+  zoom(map);
+}
+
+/**
+ * Update a map's viewport to fit each geometry in a dataset
+ */
+function zoom(map) {
+  const bounds = new google.maps.LatLngBounds();
+  map.data.forEach((feature) => {
+    processPoints(feature.getGeometry(), bounds.extend, bounds);
+  });
+  map.fitBounds(bounds);
+}
+
+/**
+ * Process each point in a Geometry, regardless of how deep the points may lie.
+ */
+function processPoints(geometry, callback, thisArg) {
+  if (geometry instanceof google.maps.LatLng) {
+    callback.call(thisArg, geometry);
+  } else if (geometry instanceof google.maps.Data.Point) {
+    callback.call(thisArg, geometry.get());
+  } else {
+    geometry.getArray().forEach((g) => {
+      processPoints(g, callback, thisArg);
+    });
+  }
+}
+
+/* DOM (drag/drop) functions */
+function initEvents() {
+  [...document.getElementsByClassName("file")].forEach((fileElement) => {
+    fileElement.addEventListener(
+      "dragstart",
+      (e) => {
+        e.dataTransfer.setData(
+          "text/plain",
+          JSON.stringify(files[Number(e.target.dataset.value)])
+        );
+        console.log(e);
+      },
+      false
+    );
+  });
+  // set up the drag & drop events
+  const mapContainer = document.getElementById("map");
+  mapContainer.addEventListener("dragenter", addClassToDropTarget, false);
+  mapContainer.addEventListener("dragover", addClassToDropTarget, false);
+  mapContainer.addEventListener("drop", handleDrop, false);
+  mapContainer.addEventListener("dragleave", removeClassFromDropTarget, false);
+}
+
+function addClassToDropTarget(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  document.getElementById("map").classList.add("over");
+  return false;
+}
+
+function removeClassFromDropTarget(e) {
+  document.getElementById("map").classList.remove("over");
+}
+
+function handleDrop(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  removeClassFromDropTarget(e);
+  const files = e.dataTransfer.files;
+
+  if (files.length) {
+    // process file(s) being dropped
+    // grab the file data from each file
+    for (let i = 0, file; (file = files[i]); i++) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        loadGeoJsonString(reader.result);
+      };
+
+      reader.onerror = function (e) {
+        console.error("reading failed");
+      };
+      reader.readAsText(file);
+    }
+  } else {
+    // process non-file (e.g. text or html) content being dropped
+    // grab the plain text version of the data
+    const plainText = e.dataTransfer.getData("text/plain");
+    console.log(plainText);
+
+    if (plainText) {
+      loadGeoJsonString(plainText);
+    }
+  }
+  // prevent drag event from bubbling further
+  return false;
+}
+
+function initialize() {
+  initMap();
+  initEvents();
+}
 const files = [
   {
     type: "FeatureCollection",
@@ -90,129 +208,4 @@ const files = [
     ],
   },
 ];
-let map;
-
-function initMap() {
-  // set up the map
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: new google.maps.LatLng(0, 0),
-    zoom: 4,
-  });
-}
-
-function loadGeoJsonString(geoString) {
-  try {
-    const geojson = JSON.parse(geoString);
-    map.data.addGeoJson(geojson);
-  } catch (e) {
-    alert("Not a GeoJSON file!");
-  }
-  zoom(map);
-}
-
-/**
- * Update a map's viewport to fit each geometry in a dataset
- * @param {google.maps.Map} map The map to adjust
- */
-function zoom(map) {
-  const bounds = new google.maps.LatLngBounds();
-  map.data.forEach((feature) => {
-    processPoints(feature.getGeometry(), bounds.extend, bounds);
-  });
-  map.fitBounds(bounds);
-}
-
-/**
- * Process each point in a Geometry, regardless of how deep the points may lie.
- * @param {google.maps.Data.Geometry} geometry The structure to process
- * @param {function(google.maps.LatLng)} callback A function to call on each
- *     LatLng point encountered (e.g. Array.push)
- * @param {Object} thisArg The value of 'this' as provided to 'callback' (e.g.
- *     myArray)
- */
-function processPoints(geometry, callback, thisArg) {
-  if (geometry instanceof google.maps.LatLng) {
-    callback.call(thisArg, geometry);
-  } else if (geometry instanceof google.maps.Data.Point) {
-    callback.call(thisArg, geometry.get());
-  } else {
-    geometry.getArray().forEach((g) => {
-      processPoints(g, callback, thisArg);
-    });
-  }
-}
-
-/* DOM (drag/drop) functions */
-function initEvents() {
-  [...document.getElementsByClassName("file")].forEach((fileElement) => {
-    fileElement.addEventListener(
-      "dragstart",
-      (e) => {
-        e.dataTransfer.setData(
-          "text/plain",
-          JSON.stringify(files[Number(e.target.dataset.value)])
-        );
-        console.log(e);
-      },
-      false
-    );
-  });
-  // set up the drag & drop events
-  const mapContainer = document.getElementById("map");
-  mapContainer.addEventListener("dragenter", addClassToDropTarget, false);
-  mapContainer.addEventListener("dragover", addClassToDropTarget, false);
-  mapContainer.addEventListener("drop", handleDrop, false);
-  mapContainer.addEventListener("dragleave", removeClassFromDropTarget, false);
-}
-
-function addClassToDropTarget(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  document.getElementById("map").classList.add("over");
-  return false;
-}
-
-function removeClassFromDropTarget(e) {
-  document.getElementById("map").classList.remove("over");
-}
-
-function handleDrop(e) {
-  e.preventDefault();
-  e.stopPropagation();
-  removeClassFromDropTarget(e);
-  const files = e.dataTransfer.files;
-
-  if (files.length) {
-    // process file(s) being dropped
-    // grab the file data from each file
-    for (let i = 0, file; (file = files[i]); i++) {
-      const reader = new FileReader();
-
-      reader.onload = function (e) {
-        loadGeoJsonString(reader.result);
-      };
-
-      reader.onerror = function (e) {
-        console.error("reading failed");
-      };
-      reader.readAsText(file);
-    }
-  } else {
-    // process non-file (e.g. text or html) content being dropped
-    // grab the plain text version of the data
-    const plainText = e.dataTransfer.getData("text/plain");
-    console.log(plainText);
-
-    if (plainText) {
-      loadGeoJsonString(plainText);
-    }
-  }
-  // prevent drag event from bubbling further
-  return false;
-}
-
-function initialize() {
-  initMap();
-  initEvents();
-}
 // [END maps_layer_data_dragndrop]
