134fa0501888c3d1e1ce460f6dbda33140458643
Justin <jpoehnelt@google.com>
2020-10-21 16:43:13 -0700

    fix: region tags moved

--- dist/samples/interaction-greedy/index.js
+++ dist/samples/interaction-greedy/index.js
@@ -2,15 +2,12 @@
 function initMap() {
   const center = { lat: -25.363, lng: 131.044 };
   const zoom = 4;
-  new google.maps.Map(
-    document.getElementById("map"),
-    // [START maps_interaction_greedy_mapoptions]
-    {
-      zoom,
-      center,
-      gestureHandling: "greedy",
-    }
-    // [END maps_interaction_greedy_mapoptions]
-  );
+  // [START maps_interaction_greedy_mapoptions]
+  new google.maps.Map(document.getElementById("map"), {
+    zoom,
+    center,
+    gestureHandling: "greedy",
+  });
+  // [END maps_interaction_greedy_mapoptions]
 }
 // [END maps_interaction_greedy]
