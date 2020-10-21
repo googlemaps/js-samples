117a6a933499c3355926c47d7143fe3922f402c0
Justin Poehnelt <jpoehnelt@google.com>
2020-10-21 16:29:44 -0700

    fix: interaction sample cleanup (#373)

--- dist/samples/interaction-restricted/index.js
+++ dist/samples/interaction-restricted/index.js
@@ -1,9 +1,4 @@
 // [START maps_interaction_restricted]
-/**
- * This sample sets the gesture handling mode to 'cooperative',
- * which means that on a mobile device, the user must swipe with one
- * finger to scroll the page and two fingers to pan the map.
- */
 function initMap() {
   const center = new google.maps.LatLng({ lat: -25.363, lng: 131.044 });
   const zoom = 4;
@@ -13,16 +8,15 @@ function initMap() {
     {
       zoom,
       center,
-      minZoom: zoom - 2,
-      maxZoom: zoom + 2,
+      minZoom: zoom - 3,
+      maxZoom: zoom + 3,
       restriction: {
         latLngBounds: {
-          north: -20,
-          south: -30,
-          east: 140,
-          west: 120,
+          north: -10,
+          south: -40,
+          east: 160,
+          west: 100,
         },
-        strictBounds: true,
       },
     }
     // [END maps_interaction_restricted_mapoptions]
--- samples/interaction-restricted/src/index.ts
+++ samples/interaction-restricted/src/index.ts
@@ -15,11 +15,6 @@
  */
 
 // [START maps_interaction_restricted]
-/**
- * This sample sets the gesture handling mode to 'cooperative',
- * which means that on a mobile device, the user must swipe with one
- * finger to scroll the page and two fingers to pan the map.
- */
 function initMap(): void {
   const center = new google.maps.LatLng({ lat: -25.363, lng: 131.044 });
   const zoom = 4;
@@ -30,16 +25,15 @@ function initMap(): void {
     {
       zoom,
       center,
-      minZoom: zoom - 2,
-      maxZoom: zoom + 2,
+      minZoom: zoom - 3,
+      maxZoom: zoom + 3,
       restriction: {
         latLngBounds: {
-          north: -20,
-          south: -30,
-          east: 140,
-          west: 120,
+          north: -10,
+          south: -40,
+          east: 160,
+          west: 100,
         },
-        strictBounds: true,
       },
     }
     // [END maps_interaction_restricted_mapoptions]
