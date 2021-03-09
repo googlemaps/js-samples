c833430befde20a72a7f818561c895fe7dac0aae
Justin Poehnelt <jpoehnelt@google.com>
2021-03-08 17:43:30 -0700

    fix: change location to feature updated 45 tilt imagery (#615)
    Co-authored-by: Alex Muramoto <amuramoto@apache.org>

--- dist/samples/aerial-simple/jsfiddle.js
+++ dist/samples/aerial-simple/jsfiddle.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 36.964, lng: -122.015 },
-    zoom: 18,
+    center: { lat: 40.7638, lng: 73.9806 },
+    zoom: 15,
     mapTypeId: "satellite",
   });
   map.setTilt(45);
