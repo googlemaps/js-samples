5332b9e3ec59094ecc3763aff7ff63eb9c5a9c93
William French <wfrench@google.com>
2021-03-23 10:14:35 -0700

    Adds new marker accessibility sample.

---
+++ dist/samples/marker-accessibility/jsfiddle.js
@@ -0,0 +1,51 @@
+// The following example creates five accessible and
+// focusable markers.
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: { lat: 34.84555, lng: -111.8035 },
+  });
+  // LatLng data for the markers.
+  const tourStops = [
+    [34.8791806, -111.8265049],
+    [34.8559195, -111.7988186],
+    [34.832149, -111.7695277],
+    [34.823736, -111.8001857],
+    [34.800326, -111.7665047],
+  ];
+  // Set title text for the markers.
+  const titleText = [
+    "The first marker (Boynton Pass) receives the initial focus when tab " +
+      "is pressed. Use arrow keys to move between markers; press tab again to " +
+      "cycle through the map controls.",
+    "Second marker: Airport Mesa",
+    "Third marker: Chapel of the Holy Cross",
+    "Fourth marker: Red Rock Crossing",
+    "Marker five: Bell Rock, the final stop on our tour!",
+  ];
+
+  for (let i = 0; i < titleText.length; ++i) {
+    const pos = tourStops[i];
+    const marker = new google.maps.Marker({
+      position: { lat: pos[0], lng: pos[1] },
+      map: map,
+    });
+    attachText(marker, titleText[i], (i + 1).toString());
+  }
+}
+
+// Adds a click listener and title text to each marker.
+function attachText(marker, titleText, labelText) {
+  // Set the marker title to provide accessible text.
+  marker.setTitle(titleText);
+  // Add a numeric label to the marker.
+  marker.setLabel(labelText);
+  // Create an info window using the same title text.
+  const infowindow = new google.maps.InfoWindow({
+    content: titleText,
+  });
+  // Set a click listener to make the marker accessible.
+  marker.addListener("click", () => {
+    infowindow.open(marker.get("map"), marker);
+  });
+}
