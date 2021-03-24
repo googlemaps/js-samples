a33b3bc7c935d19340475365712c37e1cdf385f5
William French <wfrench@google.com>
2021-03-24 09:40:05 -0700

    Incorporated more review comments; further simplification.

--- samples/marker-accessibility/src/index.ts
+++ samples/marker-accessibility/src/index.ts
@@ -28,45 +28,34 @@ function initMap(): void {
     }
   );
 
-  // LatLng data for the markers.
-  const tourStops: [number, number][] = [
-    [34.8791806, -111.8265049], // Boynton Pass
-    [34.8559195, -111.7988186], // Airport Mesa
-    [34.832149, -111.7695277], // Chapel of the Holy Cross
-    [34.823736, -111.8001857], // Red Rock Crossing
-    [34.800326, -111.7665047], // Bell Rock
-  ];
-
-  // Set title text for the markers.
-  const titleText: string[] = [
-    "The first marker (Boynton Pass) receives the initial focus when tab " +
-      "is pressed. Use arrow keys to move between markers; press tab again to " +
-      "cycle through the map controls.",
-    "Second marker: Airport Mesa",
-    "Third marker: Chapel of the Holy Cross",
-    "Fourth marker: Red Rock Crossing",
-    "Marker five: Bell Rock, the final stop on our tour!",
-  ];
+  // Set LatLng and title text for the markers. The first marker (Boynton Pass)
+  // receives the initial focus when tab is pressed. Use arrow keys to
+  // move between markers; press tab again to cycle through the map controls.
+  const tourStops: [google.maps.LatLngLiteral, string][] = [
+  	[{lat: 34.8791806, lng: -111.8265049},"Boynton Pass"],
+    [{lat: 34.8559195, lng: -111.7988186}, "Airport Mesa"],
+    [{lat: 34.832149, lng: -111.7695277}, "Chapel of the Holy Cross"],
+    [{lat:34.823736, lng: -111.8001857}, "Red Rock Crossing"],
+    [{lat: 34.800326, lng: -111.7665047}, "Bell Rock"],
+	];
 
   // Create an info window to share between markers.
-  const infowindow = new google.maps.InfoWindow();
+  const infoWindow = new google.maps.InfoWindow();
 
   // Create the markers.
-  titleText.forEach((title, i) => {
+  tourStops.forEach((item, i) => {
     const marker = new google.maps.Marker({
-      position: {
-        lat: tourStops[i][0], lng: tourStops[i][1],
-      },
+      position: item[0],
       map,
-      title,
+      title: `${i + 1}` + ". " + item[1],
       label: `${i + 1}`,
     });
     
     // Add a click listener for each marker, and set up the info window.
     marker.addListener("click", () => {
-      infowindow.close();
-      infowindow.setContent(marker.getTitle());
-      infowindow.open(marker.getMap(), marker);
+      infoWindow.close();
+      infoWindow.setContent(marker.getTitle());
+      infoWindow.open(marker.getMap(), marker);
     });
   });
 }
