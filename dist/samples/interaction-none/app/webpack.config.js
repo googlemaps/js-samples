d666f0f039c3c01b27807fe99d90b281bf552fff
Justin Poehnelt <jpoehnelt@google.com>
2021-05-21 12:31:06 -0600

    fix: devserver settings (#695)

--- dist/samples/interaction-none/app/webpack.config.js
+++ dist/samples/interaction-none/app/webpack.config.js
@@ -81,8 +81,8 @@ module.exports = (env) => {
     devServer: {
       liveReload: true,
       host: "0.0.0.0",
-      port: 8080,
-      historyApiFallback: true,
+      firewall: false,
+      hot: false,
     },
   };
 };
