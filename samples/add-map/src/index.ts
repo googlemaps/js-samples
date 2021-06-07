dd53ee6c36dafd39234c6a3b20049555f56220c1
Justin Poehnelt <jpoehnelt@google.com>
2021-06-07 21:22:33 +0000

    chore: try making change
    Change-Id: Id65654baca469d9c4bb40849a87c7b5755a89a22

--- samples/add-map/src/index.ts
+++ samples/add-map/src/index.ts
@@ -24,7 +24,7 @@ function initMap(): void {
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      zoom: 4,
+      zoom: 6,
       center: uluru,
     }
   );
