bca7a9ab0241797280ed395afd61d8a520188255
Justin Poehnelt <jpoehnelt@google.com>
2021-06-10 11:32:32 -0600

    feat: add move camera example using tweenjs (#724)

---
+++ dist/samples/move-camera-ease/index.js
@@ -0,0 +1,33 @@
+// [START maps_move_camera_ease]
+import { Tween, update, Easing } from "@tweenjs/tween.js";
+let map;
+const cameraOptions = {
+  tilt: 0,
+  heading: 0,
+  zoom: 3,
+  center: { lat: 29.9773, lng: 31.1325 },
+};
+const mapOptions = {
+  ...cameraOptions,
+  mapId: "15431d2b469f209e",
+};
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  // install Tweenjs with npm i @tweenjs/tween.js
+  new Tween(cameraOptions) // Create a new tween that modifies 'cameraOptions'.
+    .to({ tilt: 65, heading: -90, zoom: 18 }, 15000) // Move to destination in 15 second.
+    .easing(Easing.Quadratic.Out) // Use an easing function to make the animation smooth.
+    .onUpdate(() => {
+      map.moveCamera(cameraOptions);
+    })
+    .start(); // Start the tween immediately.
+
+  // Setup the animation loop.
+  function animate(time) {
+    requestAnimationFrame(animate);
+    update(time);
+  }
+  requestAnimationFrame(animate);
+}
+// [END maps_move_camera_ease]
