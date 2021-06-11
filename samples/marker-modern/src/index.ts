b6af3e0bfe96019943f8dc1a495cc7af2a4683f6
Justin Poehnelt <jpoehnelt@google.com>
2021-06-11 17:27:03 +0000

    fix; change location again
    Change-Id: I49edf7b668fc2ba1acd8f94072cf1273c83b0db6

--- samples/marker-modern/src/index.ts
+++ samples/marker-modern/src/index.ts
@@ -18,19 +18,19 @@
 import { faBus } from "@fortawesome/free-solid-svg-icons";
 
 function initMap(): void {
-  const center = { lat: 40.7812, lng: 73.9665 };
+  const center = { lat: 36.6163, lng: -100.605 };
 
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      zoom: 9,
+      zoom: 15,
       center,
     }
   );
 
   // use a FontAwesome svg
   new google.maps.Marker({
-    position: { lat: 40.8, lng: 73.9665 },
+    position: { lat: 36.6163, lng: -100.61 },
     map,
     icon: {
       path: faBus.icon[4] as string,
@@ -49,7 +49,7 @@ function initMap(): void {
 
   // use a Material Icon as font
   new google.maps.Marker({
-    position: { lat: 40.7812, lng: 73.9665 },
+    position: { lat: 36.6163, lng: -100.6 },
     map,
     label: {
       text: "\ue530", // codepoint from https://fonts.google.com/icons
