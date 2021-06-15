0e300179d3937f0294b59d4bd37f8149b5e7c25c
Justin Poehnelt <jpoehnelt@google.com>
2021-06-15 12:28:41 -0600

    fix: distance matrix icons

--- dist/samples/distance-matrix/jsfiddle.js
+++ dist/samples/distance-matrix/jsfiddle.js
@@ -1,86 +1,77 @@
 function initMap() {
   const bounds = new google.maps.LatLngBounds();
   const markersArray = [];
-  const origin1 = { lat: 55.93, lng: -3.118 };
-  const origin2 = "Greenwich, England";
-  const destinationA = "Stockholm, Sweden";
-  const destinationB = { lat: 50.087, lng: 14.421 };
-  const destinationIcon =
-    "https://chart.googleapis.com/chart?" +
-    "chst=d_map_pin_letter&chld=D|FF0000|000000";
-  const originIcon =
-    "https://chart.googleapis.com/chart?" +
-    "chst=d_map_pin_letter&chld=O|FFFF00|000000";
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 55.53, lng: 9.4 },
     zoom: 10,
   });
+  // initialize services
   const geocoder = new google.maps.Geocoder();
   const service = new google.maps.DistanceMatrixService();
-  service.getDistanceMatrix(
-    {
-      origins: [origin1, origin2],
-      destinations: [destinationA, destinationB],
-      travelMode: google.maps.TravelMode.DRIVING,
-      unitSystem: google.maps.UnitSystem.METRIC,
-      avoidHighways: false,
-      avoidTolls: false,
-    },
-    (response, status) => {
-      if (status !== "OK") {
-        alert("Error was: " + status);
-      } else {
-        const originList = response.originAddresses;
-        const destinationList = response.destinationAddresses;
-        const outputDiv = document.getElementById("output");
-        outputDiv.innerHTML = "";
-        deleteMarkers(markersArray);
+  // build request
+  const origin1 = { lat: 55.93, lng: -3.118 };
+  const origin2 = "Greenwich, England";
+  const destinationA = "Stockholm, Sweden";
+  const destinationB = { lat: 50.087, lng: 14.421 };
+  const request = {
+    origins: [origin1, origin2],
+    destinations: [destinationA, destinationB],
+    travelMode: google.maps.TravelMode.DRIVING,
+    unitSystem: google.maps.UnitSystem.METRIC,
+    avoidHighways: false,
+    avoidTolls: false,
+  };
+  // put request on page
+  document.getElementById("request").innerText = JSON.stringify(
+    request,
+    null,
+    2
+  );
+  // get distance matrix response
+  service.getDistanceMatrix(request).then((response) => {
+    // put response
+    document.getElementById("response").innerText = JSON.stringify(
+      response,
+      null,
+      2
+    );
+    // show on map
+    const originList = response.originAddresses;
+    const destinationList = response.destinationAddresses;
+    deleteMarkers(markersArray);
 
-        const showGeocodedAddressOnMap = function (asDestination) {
-          const icon = asDestination ? destinationIcon : originIcon;
-
-          return function (results, status) {
-            if (status === "OK") {
-              map.fitBounds(bounds.extend(results[0].geometry.location));
-              markersArray.push(
-                new google.maps.Marker({
-                  map,
-                  position: results[0].geometry.location,
-                  icon: icon,
-                })
-              );
-            } else {
-              alert("Geocode was not successful due to: " + status);
-            }
-          };
-        };
-
-        for (let i = 0; i < originList.length; i++) {
-          const results = response.rows[i].elements;
-          geocoder.geocode(
-            { address: originList[i] },
-            showGeocodedAddressOnMap(false)
+    const showGeocodedAddressOnMap = function (asDestination) {
+      return function (results, status) {
+        if (status === "OK") {
+          map.fitBounds(bounds.extend(results[0].geometry.location));
+          markersArray.push(
+            new google.maps.Marker({
+              map,
+              position: results[0].geometry.location,
+              label: asDestination ? "D" : "O",
+            })
           );
-
-          for (let j = 0; j < results.length; j++) {
-            geocoder.geocode(
-              { address: destinationList[j] },
-              showGeocodedAddressOnMap(true)
-            );
-            outputDiv.innerHTML +=
-              originList[i] +
-              " to " +
-              destinationList[j] +
-              ": " +
-              results[j].distance.text +
-              " in " +
-              results[j].duration.text +
-              "<br>";
-          }
+        } else {
+          alert("Geocode was not successful due to: " + status);
         }
+      };
+    };
+
+    for (let i = 0; i < originList.length; i++) {
+      const results = response.rows[i].elements;
+      geocoder.geocode(
+        { address: originList[i] },
+        showGeocodedAddressOnMap(false)
+      );
+
+      for (let j = 0; j < results.length; j++) {
+        geocoder.geocode(
+          { address: destinationList[j] },
+          showGeocodedAddressOnMap(true)
+        );
       }
     }
-  );
+  });
 }
 
 function deleteMarkers(markersArray) {
