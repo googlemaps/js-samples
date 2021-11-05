570cc147e8d9a6d352cffe0b71eee6f1226151dc
Justin Poehnelt <jpoehnelt@google.com>
2021-11-05 14:11:25 -0600

    fix: add click listener to make navigable by keyboard (#956)

--- samples/marker-clustering/src/index.ts
+++ samples/marker-clustering/src/index.ts
@@ -25,15 +25,30 @@ function initMap(): void {
     }
   );
 
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
+  });
+
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   // Add some markers to the map.
-  const markers = locations.map((location, i) => {
-    return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length],
+  const markers = locations.map((position, i) => {
+    const label = labels[i % labels.length];
+    const marker = new google.maps.Marker({
+      position,
+      label,
     });
+
+    // markers can only be keyboard focusable when they have click listeners
+    // open info window when marker is clicked
+    marker.addListener("click", () => {
+      infoWindow.setContent(label);
+      infoWindow.open(map, marker);
+    });
+
+    return marker;
   });
 
   // Add a marker clusterer to manage the markers.
