352117b39641be308cd0df73b1b892af21595425
Justin Poehnelt <jpoehnelt@google.com>
2022-02-10 22:30:00 -0700

    chore: ignore resizer js

--- dist/resizer.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("message",(e=>{(e=>e.data&&"resizeMessage"===e.data.type)(e)&&(document.getElementById("iframe").style.height=e.data.rect.height+10+"px",window.devsite.framebox.AutoSizeClient.updateSize())}));
-//# sourceMappingURL=resizer.js.map
