1539dba451e9af094981dc335c873037df36a291
Justin Poehnelt <jpoehnelt@google.com>
2022-02-11 16:29:11 -0700

    test: improve playwright test performance (#1007)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,27 @@
-import { PlaywrightTestConfig } from "@playwright/test";
+import { PlaywrightTestConfig, devices } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   testDir: "e2e",
-  timeout: 10000,
-  workers: 1,
+  retries: 2,
+  timeout: process.env.CI ? 10000 : 5000,
+  workers: process.env.CI ? 2 : undefined,
   use: {
     viewport: {
       width: 600,
       height: 1200,
     },
-     baseURL: 'http://localhost:8000/',
+    baseURL: "http://localhost:8000/",
   },
-     webServer: {       
-       command: 'npm run serve',
-       port: 8000,
-     },
+  webServer: {
+    command: "npm run serve",
+    port: 8000,
+  },
   updateSnapshots: "none",
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+  reporter: process.env.CI ? "github" : "list",
 };
 export default config;
