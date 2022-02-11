fbb019b1d9249b8d4f16104b2d52459dfaa546da
Justin Poehnelt <jpoehnelt@google.com>
2022-02-11 15:09:55 -0700

    test: retires and debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,26 @@
-import { PlaywrightTestConfig } from "@playwright/test";
+import { PlaywrightTestConfig, devices } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   testDir: "e2e",
-  timeout: process.env.CI ? 30000 : 10000,
+  retries: 2,
+  timeout: process.env.CI ? 10000 : 5000,
   workers: process.env.CI ? 1 : undefined,
   use: {
     viewport: {
       width: 600,
       height: 1200,
     },
-     baseURL: 'http://localhost:8000/',
+    baseURL: "http://localhost:8000/",
   },
-     webServer: {       
-       command: 'npm run serve',
-       port: 8000,
-     },
+  webServer: {
+    command: "npm run serve",
+    port: 8000,
+  },
   updateSnapshots: "none",
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
 };
 export default config;
