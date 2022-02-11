aa298b20f8728229dee77b077d46081153a71a35
Justin Poehnelt <jpoehnelt@google.com>
2022-02-11 13:40:11 -0700

    test: allow more worker processes

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   testDir: "e2e",
   timeout: 10000,
-  workers: 1,
+  workers: process.env.CI ? 4 : undefined,
   use: {
     viewport: {
       width: 600,
