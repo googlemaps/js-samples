d9d4bfeb853f04b81e719fd7c456f06c1156bbbe
Justin Poehnelt <jpoehnelt@google.com>
2022-04-08 10:48:27 -0600

    feat: build iframes with vite and plugin

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,11 @@ const yourAPIKey = require("./src/transforms/your-api-key");
 const format = require("./src/transforms/format");
 const minify = require("./src/transforms/minify");
 const skypack = require("./src/transforms/skypack");
+const fs = require("fs");
+const path = require("path");
+const vite = require("vite");
+const chalk = require("chalk");
+const { rm } = require("fs/promises");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./shared/**/*");
@@ -37,6 +42,88 @@ module.exports = function (eleventyConfig) {
     }
 
     return samples;
+  });
+
+  // build sample iframes after building the site using vite and a plugin to
+  // inline assets such as css and js
+  eleventyConfig.on("eleventy.after", async () => {
+    console.log(chalk.green("[11ty.after] Building dist/samples/*/app"));
+
+    const samplesPath = path.join(__dirname, "dist", "samples");
+
+    // get samples in dist folder
+    const samples = fs
+      .readdirSync(samplesPath, {
+        withFileTypes: true,
+      })
+      .filter((d) => d.isDirectory())
+      .map((d) => d.name);
+
+    // remove warning https://stackoverflow.com/questions/8313628/node-js-request-how-to-emitter-setmaxlisteners
+    require("events").EventEmitter.defaultMaxListeners = samples.length * 2;
+
+    await Promise.all(
+      samples.map((sample) =>
+        vite.build({
+          plugins: [
+            {
+              name: "vite:iframe",
+              transformIndexHtml: {
+                enforce: "post",
+                transform(html, ctx) {
+                  if (!ctx || !ctx.bundle) return html;
+
+                  for (const [, value] of Object.entries(ctx.bundle)) {
+                    const o = value;
+                    const a = value;
+                    if (o.code) {
+                      const reScript = new RegExp(
+                        `<script type="module"[^>]*?src="[./]*${o.fileName}"[^>]*?></script>`
+                      );
+                      const code = `<script type="module">\n//${o.fileName}\n${o.code}\n</script>`;
+                      html = html.replace(reScript, () => code);
+                    } else if (a.fileName.endsWith(".css")) {
+                      const reCSS = new RegExp(
+                        `<link rel="stylesheet"[^>]*?href="[./]*${a.fileName}"[^>]*?>`
+                      );
+                      const code = `<style type="text/css">\n${a.source}\n</style>`;
+                      html = html.replace(reCSS, () => code);
+                    } else {
+                      throw new Error(`asset not inlined: ${a.fileName}`);
+                    }
+                  }
+
+                  return html;
+                },
+              },
+            },
+          ],
+          root: path.join(samplesPath, sample, "app"),
+          base: "./",
+          logLevel: "error",
+          build: {
+            target: "es2015",
+            assetsInlineLimit: 100000000,
+            cssCodeSplit: false,
+            outDir: path.join("../", "iframe"),
+          },
+        })
+      )
+    );
+
+    // remove assets directories since plugin inlined assets
+    await Promise.all(
+      samples.map((sample) =>
+        rm(path.join(samplesPath, sample, "iframe", "assets"), {
+          recursive: true,
+          force: true,
+        })
+      )
+    );
+
+    console.log(
+      chalk.green("[11ty.after] Finished building dist/samples/*/app")
+    );
   });
 
   return {
