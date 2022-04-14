a07f5c542508f37dca2960e51f481d833bd700ae
Justin Poehnelt <jpoehnelt@google.com>
2022-04-14 12:30:21 -0600

    feat: add map-events sample

---
+++ dist/samples/map-events/docs/index.js
@@ -0,0 +1,73 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// [START maps_map_events]
+// [START maps_map_event]
+const events = [
+  "bounds_changed",
+  "center_changed",
+  "click",
+  "contextmenu",
+  "dblclick",
+  "drag",
+  "dragend",
+  "dragstart",
+  "heading_changed",
+  "idle",
+  "maptypeid_changed",
+  "mousemove",
+  "mouseout",
+  "mouseover",
+  "projection_changed",
+  "resize",
+  "rightclick",
+  "tilesloaded",
+  "tilt_changed",
+  "zoom_changed",
+];
+
+function setupListener(map, name) {
+  const eventRow = document.getElementById(name);
+
+  google.maps.event.addListener(map, name, () => {
+    eventRow.className = "event active";
+
+    const timeout = setTimeout(() => {
+      eventRow.className = "event inactive";
+    }, 1000);
+  });
+}
+
+function initMap() {
+  populateTable();
+
+  const mapDiv = document.getElementById("map");
+  const map = new google.maps.Map(mapDiv, {
+    center: new google.maps.LatLng(37.4419, -122.1419),
+    zoom: 13,
+    mapTypeId: "roadmap",
+  });
+
+  for (let i = 0; i < events.length; i++) {
+    setupListener(map, events[i]);
+  }
+}
+
+// Dynamically create the table of events from the defined hashmap
+function populateTable() {
+  const eventsTable = document.getElementById("events");
+  let content = "";
+
+  for (let i = 0; i < events.length; i++) {
+    content +=
+      '<div class="event" id="' + events[i] + '">' + events[i] + "</div>";
+  }
+
+  eventsTable.innerHTML = content;
+}
+
+window.initMap = initMap;
+// [END maps_map_event]
+// [END maps_map_events]
