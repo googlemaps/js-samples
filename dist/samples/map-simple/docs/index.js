c858d5d5663b6c07ddba9515a80b8613f2e605f9
Justin Poehnelt <jpoehnelt@google.com>
2022-05-24 21:06:45 +0000

    chore: change zoom

--- dist/samples/map-simple/docs/index.js
+++ dist/samples/map-simple/docs/index.js
@@ -9,7 +9,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    zoom: 12,
   });
 }
 
--- dist/samples/map-simple/playground/index.ts
+++ dist/samples/map-simple/playground/index.ts
@@ -3,7 +3,7 @@ let map: google.maps.Map;
 function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    zoom: 12,
   });
 }
 
