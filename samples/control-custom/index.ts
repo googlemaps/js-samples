0c3774157e9960f973b89dbe8b3ddffb307367aa
William French <wfrench@google.com>
2022-08-24 14:36:10 -0700

    fix:Updates custom control sample to be keyboard-accessible.
    Change-Id: I1904dae8db70088ed8ba65cd8f264eb58bdd3c65

--- samples/control-custom/index.ts
+++ samples/control-custom/index.ts
@@ -10,56 +10,50 @@ let map: google.maps.Map;
 const chicago = { lat: 41.85, lng: -87.65 };
 
 /**
- * The CenterControl adds a control to the map that recenters the map on
- * Chicago.
- * This constructor takes the control DIV as an argument.
- * @constructor
+ * Creates a control that recenters the map on Chicago.
  */
-function CenterControl(controlDiv: Element, map: google.maps.Map) {
-  // Set CSS for the control border.
-  const controlUI = document.createElement("div");
+ function createCenterControl(map) {
+  const controlButton = document.createElement('button');
 
-  controlUI.style.backgroundColor = "#fff";
-  controlUI.style.border = "2px solid #fff";
-  controlUI.style.borderRadius = "3px";
-  controlUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
-  controlUI.style.cursor = "pointer";
-  controlUI.style.marginTop = "8px";
-  controlUI.style.marginBottom = "22px";
-  controlUI.style.textAlign = "center";
-  controlUI.title = "Click to recenter the map";
-  controlDiv.appendChild(controlUI);
-
-  // Set CSS for the control interior.
-  const controlText = document.createElement("div");
-
-  controlText.style.color = "rgb(25,25,25)";
-  controlText.style.fontFamily = "Roboto,Arial,sans-serif";
-  controlText.style.fontSize = "16px";
-  controlText.style.lineHeight = "38px";
-  controlText.style.paddingLeft = "5px";
-  controlText.style.paddingRight = "5px";
-  controlText.innerHTML = "Center Map";
-  controlUI.appendChild(controlText);
+  // Set CSS for the control.
+  controlButton.style.backgroundColor = '#fff';
+  controlButton.style.border = '2px solid #fff';
+  controlButton.style.borderRadius = '3px';
+  controlButton.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlButton.style.color = 'rgb(25,25,25)';
+  controlButton.style.cursor = 'pointer';
+  controlButton.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlButton.style.fontSize = '16px';
+  controlButton.style.lineHeight = '38px';
+  controlButton.style.margin = '8px 0 22px';
+  controlButton.style.padding = '0 5px';
+  controlButton.style.textAlign = 'center';
+  
+  controlButton.textContent = 'Center Map';
+  controlButton.title = 'Click to recenter the map';
+  controlButton.type = 'button';
 
   // Setup the click event listeners: simply set the map to Chicago.
-  controlUI.addEventListener("click", () => {
+  controlButton.addEventListener('click', () => {
     map.setCenter(chicago);
   });
+  
+  return controlButton;
 }
 
-function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
     zoom: 12,
     center: chicago,
   });
 
-  // Create the DIV to hold the control and call the CenterControl()
-  // constructor passing in this DIV.
-  const centerControlDiv = document.createElement("div");
-
-  CenterControl(centerControlDiv, map);
-
+  // Create the DIV to hold the control.
+  const centerControlDiv = document.createElement('div');
+  // Create the control.
+  const centerControl = createCenterControl(map);
+  // Append the control to the DIV.
+  centerControlDiv.appendChild(centerControl);
+  
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
 }
 
