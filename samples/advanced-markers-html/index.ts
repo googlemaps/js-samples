b4eecc20fdae9363dba0118ea1a837efdc0e7f9e
William French <wfrench@google.com>
2022-10-18 12:14:07 -0700

    fix: Fixes element
    The element incorrectly used `content`; it should be `element`.
    
    /update-dist

--- samples/advanced-markers-html/index.ts
+++ samples/advanced-markers-html/index.ts
@@ -24,7 +24,7 @@ function initMap() {
       position: property.position,
       title: property.description,
     });
-    const element = advancedMarkerView.content as HTMLElement;
+    const element = advancedMarkerView.element as HTMLElement;
     ["focus", "pointerenter"].forEach((event) => {
       element.addEventListener(event, () => {
         highlight(advancedMarkerView, property);
