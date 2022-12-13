f227044a5991cdf0bac637e4905efcd85a85a72b
amuramoto <amuramoto@users.noreply.github.com>
2022-12-13 19:49:09 +0000

    deploy: c2f13666e261670df7e0927c12478651b1b1ff90

--- samples/advanced-markers-altitude/playground/index.ts
+++ samples/advanced-markers-altitude/playground/index.ts
@@ -18,7 +18,11 @@ function initMap() {
     map,
     content: pinView.element,
     // Set altitude to 20 meters above the ground.
-    position: { lat: 47.65170843460547, lng: -122.30754, altitude: 20 },
+    position: {
+      lat: 47.65170843460547,
+      lng: -122.30754,
+      altitude: 20,
+    } as google.maps.LatLngAltitudeLiteral,
   });
 }
 
