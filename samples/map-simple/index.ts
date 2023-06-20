a75242440dd56ff3a4044a25b20d7d74cebf14db
William French <wfrench@google.com>
2023-06-20 14:54:37 -0700

    Update index.ts
    Removes an extra closing semicolon.

--- samples/map-simple/index.ts
+++ samples/map-simple/index.ts
@@ -7,7 +7,7 @@
 // [START maps_map_simple]
 let map: google.maps.Map;
 async function initMap(): Promise<void> {
-  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;;
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
   map = new Map(document.getElementById("map") as HTMLElement, {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
