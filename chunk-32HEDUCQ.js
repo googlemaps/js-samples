a5ab5f19891f188aabd81170111b8ab67e9e1a64
willum070 <willum070@users.noreply.github.com>
2023-07-11 17:51:47 +0000

    deploy: 7fd3e4f8cef72eceb03923b17fffaa58d2a8f0c7

--- chunk-32HEDUCQ.js
+++ chunk-32HEDUCQ.js
@@ -1,2 +1,2 @@
 var h=Object.defineProperty;var i=Object.getOwnPropertyDescriptor;var j=(c,a,b)=>a in c?h(c,a,{enumerable:!0,configurable:!0,writable:!0,value:b}):c[a]=b;var k=(c,a,b,e)=>{for(var d=e>1?void 0:e?i(a,b):a,f=c.length-1,g;f>=0;f--)(g=c[f])&&(d=(e?g(a,b,d):g(d))||d);return e&&d&&h(a,b,d),d};var l=(c,a,b)=>(j(c,typeof a!="symbol"?a+"":a,b),b);export{k as a,l as b};
-/*! For license information please see chunk-3M2S4UTW.js.LEGAL.txt */
+/*! For license information please see chunk-32HEDUCQ.js.LEGAL.txt */
--- samples/marker-clustering/docs/index.js
+++ samples/marker-clustering/docs/index.js
@@ -6,10 +6,16 @@
 import { MarkerClusterer } from "@googlemaps/markerclusterer";
 
 // [START maps_marker_clustering]
-function initMap() {
+async function initMap() {
+  // Request needed libraries.
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+    "marker"
+  );
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
     center: { lat: -28.024, lng: 140.887 },
+    mapId: "DEMO_MAP_ID",
   });
   const infoWindow = new google.maps.InfoWindow({
     content: "",
@@ -20,15 +26,19 @@ function initMap() {
   // Add some markers to the map.
   const markers = locations.map((position, i) => {
     const label = labels[i % labels.length];
-    const marker = new google.maps.Marker({
+    const pinGlyph = new google.maps.marker.PinElement({
+      glyph: label,
+      glyphColor: "white",
+    });
+    const marker = new google.maps.marker.AdvancedMarkerElement({
       position,
-      label,
+      content: pinGlyph.element,
     });
 
     // markers can only be keyboard focusable when they have click listeners
     // open info window when marker is clicked
     marker.addListener("click", () => {
-      infoWindow.setContent(label);
+      infoWindow.setContent(position.lat + ", " + position.lng);
       infoWindow.open(map, marker);
     });
     return marker;
@@ -64,5 +74,5 @@ const locations = [
   { lat: -43.999792, lng: 170.463352 },
 ];
 
-window.initMap = initMap;
+initMap();
 // [END maps_marker_clustering]
