d20653689cc076652c4b9477ce6b584b8c411e07
William French <wfrench@google.com>
2023-11-16 14:03:40 -0800

    Update index.ts

--- samples/web-components-events/index.ts
+++ samples/web-components-events/index.ts
@@ -7,27 +7,27 @@
 // [START maps_web_components_events]
 // This example adds a map using web components.
 function initMap(): void {
-    console.log('Maps JavaScript API loaded.');
-      const advancedMarkers = document.querySelectorAll("#marker-click-event-example gmp-advanced-marker");
-      for (const advancedMarker of advancedMarkers) {
-        
-        customElements.whenDefined(advancedMarker.localName).then(async () => {
-          advancedMarker.addEventListener('gmp-click', async () => {
-            
-            const infoWindow = new google.maps.InfoWindow({
-              content: advancedMarker.title,
-            });
-            infoWindow.open({
-              anchor: advancedMarker
-            });
-          });
+  console.log('Maps JavaScript API loaded.');
+  const advancedMarkers = document.querySelectorAll("#marker-click-event-example gmp-advanced-marker");
+  for (const advancedMarker of advancedMarkers) {
+
+    customElements.whenDefined(advancedMarker.localName).then(async () => {
+      advancedMarker.addEventListener('gmp-click', async () => {
+
+        const infoWindow = new google.maps.InfoWindow({
+          content: advancedMarker.title,
         });
-      }
+        infoWindow.open({
+          anchor: advancedMarker
+        });
+      });
+    });
+  }
 }
 
 declare global {
   interface Window {
-      initMap: () => void;
+    initMap: () => void;
   }
 }
 window.initMap = initMap;
