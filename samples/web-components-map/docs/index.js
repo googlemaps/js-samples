f2e44cf5fd38d549040969577ff7e3b091673f50
willum070 <willum070@users.noreply.github.com>
2023-11-29 17:08:24 +0000

    deploy: 2ce7ad3c44fac00f0d28cb75c82b5bcd69f23c2b

---
+++ samples/web-components-map/docs/index.js
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// [START maps_web_components_map]
+// This example adds a map using web components.
+function initMap() {
+  console.log("Maps JavaScript API loaded.");
+}
+
+window.initMap = initMap;
+// [END maps_web_components_map]
