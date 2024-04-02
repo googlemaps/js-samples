020117aab61a5935c9c1ba0f708f63bc4e145346
Teresa Qin <154479823+teresarwqin@users.noreply.github.com>
2024-04-03 10:11:05 +1100

    Update index.js
    Remove EXCLUDE label
    Set mapId as DEMO_MAP_ID for demo purpose

--- dist/samples/place-text-search/docs/index.js
+++ dist/samples/place-text-search/docs/index.js
@@ -14,7 +14,7 @@ async function initMap() {
   map = new Map(document.getElementById("map"), {
     center: center,
     zoom: 14,
-    mapId: "4504f8b37365c3d0"
+    mapId: "DEMO_MAP_ID"
   });
   findPlaces();
 }
