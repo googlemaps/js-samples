b16b74fee984b845068e7e0e4304d4d03debae82
kwnevarez <kwnevarez@users.noreply.github.com>
2024-05-01 00:14:16 +0000

    deploy: 850f386c7bbf51cd3014951e06ce39e1dc6677e8

--- samples/rgm-basic-map/playground/index.tsx
+++ samples/rgm-basic-map/playground/index.tsx
@@ -19,13 +19,14 @@ import {createRoot} from 'react-dom/client';
 import {APIProvider, Map} from '@vis.gl/react-google-maps';
 
 const API_KEY =
-  globalThis.GOOGLE_MAPS_API_KEY ?? (process.env.GOOGLE_MAPS_API_KEY as string);
+  globalThis.GOOGLE_MAPS_API_KEY ?? ("YOUR_API_KEY");
+  console.log('API_KEY',API_KEY);
 
 const App = () => (
   <APIProvider apiKey={API_KEY}>
     <Map
-      defaultZoom={3}
-      defaultCenter={{lat: 22.54992, lng: 0}}
+      defaultZoom={8}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
       gestureHandling={'greedy'}
       disableDefaultUI={true}
     />
