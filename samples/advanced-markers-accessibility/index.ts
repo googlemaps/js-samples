245164c139bca68cac1a24a70eb7d614e19379aa
William French <wfrench@google.com>
2024-05-02 10:45:33 -0700

    Update index.ts

--- samples/advanced-markers-accessibility/index.ts
+++ samples/advanced-markers-accessibility/index.ts
@@ -51,14 +51,14 @@ async function initMap() {
             glyph: `${i + 1}`,
             scale: 1.5,
         });
-
+        // [START maps_advanced_markers_accessibility_marker]
         const marker = new AdvancedMarkerElement({
             position,
             map,
             title: `${i + 1}. ${title}`,
             content: pin.element,
         });
-
+        // [END maps_advanced_markers_accessibility_marker]
         // [START maps_advanced_markers_accessibility_event_listener]
         // Add a click listener for each marker, and set up the info window.
         marker.addListener('click', ({ domEvent, latLng }) => {
