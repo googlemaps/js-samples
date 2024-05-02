e4663f11df68c2084ed9f2fa9974d66d9a625cb3
William French <wfrench@google.com>
2024-05-02 10:40:25 -0700

    fix: Adds region tags to support documentation
    Adds some new region tags for docs, and adds scale attribute.

--- samples/advanced-markers-accessibility/index.ts
+++ samples/advanced-markers-accessibility/index.ts
@@ -49,15 +49,16 @@ async function initMap() {
     tourStops.forEach(({position, title}, i) => {
         const pin = new PinElement({
             glyph: `${i + 1}`,
+            scale: 1.5;
         });
-
+        // [START maps_advanced_markers_accessibility_marker]
         const marker = new AdvancedMarkerElement({
             position,
             map,
             title: `${i + 1}. ${title}`,
             content: pin.element,
         });
-
+        // [START maps_advanced_markers_accessibility_marker]
         // Add a click listener for each marker, and set up the info window.
         marker.addListener('click', ({ domEvent, latLng }) => {
             const { target } = domEvent;
