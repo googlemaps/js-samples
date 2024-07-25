788d4c9ab178edbbfb9ed6895782a48c979c0792
William French <wfrench@google.com>
2024-07-25 11:51:00 -0700

    fix: Update index.ts with reviewer comments
    Updated the TS file with Tom's suggestions.

--- samples/place-reviews/index.ts
+++ samples/place-reviews/index.ts
@@ -35,7 +35,7 @@ async function initMap() {
     });
 
     // If there are any reviews display the first one.
-    if (place.reviews) {
+    if (place.reviews && place.reviews.length > 0) {
         // Get info for the first review.
         let reviewRating = place.reviews[0].rating;
         let reviewText = place.reviews[0].text;
@@ -43,12 +43,12 @@ async function initMap() {
         let authorUri = place.reviews[0].authorAttribution!.uri;
 
         // Format the review using HTML.
-        contentString =
-            '<div id="title"><b>' + place.displayName + '</b></div>' +
-            '<div id="address">' + place.formattedAddress + '</div>' +
-            '<a href="' + authorUri + '" target="_blank">Author: ' + authorName + '</a>' +
-            '<div id="rating">Rating: ' + reviewRating + ' stars</div>' +
-            '<div id="rating"><p>Review: ' + reviewText + '</p></div>';
+        contentString =`
+            <div id="title"><b>${place.displayName}</b></div>
+            <div id="address">${place.formattedAddress}</div>
+            <a href="${authorUri}" target="_blank">Author: ${authorName}</a>
+            <div id="rating">Rating: ${reviewRating} stars</div>
+            <div id="rating"><p>Review: ${reviewText}</p></div>`;
     } else {
         contentString = 'No reviews were found for ' + place.displayName + '.';
     }
