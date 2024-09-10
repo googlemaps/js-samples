4ee6f0c1f9779399874372ba591da8d8b34c1d94
William French <wfrench@google.com>
2024-09-10 11:59:31 -0700

    fix: Update index.ts
    Test to see what happens if the //@ts-ignore statement is removed. We shouldn't need it and I was wondering why it's there to begin with (no comment was made in any previous commit).

--- samples/add-map/index.ts
+++ samples/add-map/index.ts
@@ -13,7 +13,6 @@ async function initMap(): Promise<void> {
   const position = { lat: -25.344, lng: 131.031 };
 
   // Request needed libraries.
-  //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
 
